742c7657d64921d7056333a98f7747f121d69ed3
Shivahari S <shivaharipvt@gmail.com>
2025-07-31 09:30:34 +0530

    Create 0067 - Add Binary.java

---
+++ 0067 - Add Binary.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry=0;
+        while(i>=0 || j>=0 || carry>0){    
+            if (i >= 0) carry+= a.charAt(i--) - '0';
+            if (j >= 0) carry += b.charAt(j--) - '0';
+            res.append(carry % 2);
+            carry/= 2;
+        }
+        return res.reverse().toString();
+    }
+}
